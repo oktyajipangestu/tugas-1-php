58338b6efffc5806a522220b211630ceebaa9892
oktyajipangestu <86592837+oktyajipangestu@users.noreply.github.com>
2021-07-06 21:31:11 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,82 @@
+<?php 
+
+$ayam_goreng = 13000;
+$ayam_bakar = 15000;
+$ayam_sayur = 13000;
+$tempe_goreng = 1000;
+$tahu_goreng = 1000;
+$soto = 3000;
+$nasi_putih = 5000;
+$es_teh = 4000;
+$es_tebu = 5000;
+
+echo "
+<h1>Daftar Menu</h1>
+<table>
+    <tr>
+        <td>
+            <li>Ayam Goreng</li>
+        </td>
+        <td>=</td>
+        <td>Rp. $ayam_goreng</td>
+    </tr>
+    <tr>
+        <td>
+            <li>Ayam Bakar</li>
+        </td>
+        <td>=</td>
+        <td>Rp. $ayam_bakar</td>
+    </tr>
+    <tr>
+        <td>
+            <li>Ayam Sayur</li>
+        </td>
+        <td>=</td>
+        <td>Rp. $ayam_sayur</td>
+    </tr>
+    <tr>
+        <td>
+            <li>Tempe Goreng</li>
+        </td>
+        <td>=</td>
+        <td>Rp. $tempe_goreng</td>
+    </tr>
+    <tr>
+        <td>
+            <li>Tahu Goreng</li>
+        </td>
+        <td>=</td>
+        <td>Rp. $tahu_goreng</td>
+    </tr>
+    <tr>
+        <td>
+            <li>Soto</li>
+        </td>
+        <td>=</td>
+        <td>Rp. $soto</td>
+    </tr>
+    <tr>
+        <td>
+            <li>Nasi Putih</li>
+        </td>
+        <td>=</td>
+        <td>Rp. $nasi_putih</td>
+    </tr>
+    <tr>
+        <td>
+            <li>Es Teh</li>
+        </td>
+        <td>=</td>
+        <td>Rp. $es_teh</td>
+    </tr>
+    <tr>
+        <td>
+            <li>Es Tebu</li>
+        </td>
+        <td>=</td>
+        <td>Rp. $es_tebu</td>
+    </tr>
+</table>
+";
+
+?>
---
+++ tugas1_2.php
@@ -0,0 +1,49 @@
+<?php 
+
+$cireng = 1000;
+$es_duren = 15000;
+$es_milo = 20000;
+$es_teh = 5000;
+$piscok = 1000;
+
+echo "
+<h1>DAFTAR MENU</h1>
+<table border='1'>
+    <thead>
+        <tr>
+            <td>No.</td>
+            <td>Nama Menu</td>
+            <td>Harga</td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Cireng</td>
+            <td>Rp. $cireng</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Es Duren</td>
+            <td>Rp. $es_duren</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Es Milo</td>
+            <td>Rp. $es_milo</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Es Teh</td>
+            <td>Rp. $es_teh</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Piscok</td>
+            <td>Rp. $piscok</td>
+        </tr>
+    </tbody>
+</table>
+";
+
+?>
